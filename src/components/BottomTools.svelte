b3a2602ab56f04c038441397495d05e8628d4849
MrMelon54 <github@mrmelon54.com>
2023-05-04 16:25:30 +0100

    Add icons to the tool buttons

--- src/components/BottomTools.svelte
+++ src/components/BottomTools.svelte
@@ -1,27 +1,43 @@
+<script lang="ts">
+  import loadIcon from "~/assets/load.png";
+  import leftIcon from "~/assets/left.png";
+  import rightIcon from "~/assets/right.png";
+  import searchIcon from "~/assets/search.png";
+  import addIcon from "~/assets/add.png";
+  import addmultipleIcon from "~/assets/addmultiple.png";
+  import renameIcon from "~/assets/rename.png";
+  import reorderIcon from "~/assets/reorder.png";
+  import copypartsIcon from "~/assets/copyparts.png";
+  import copytextIcon from "~/assets/copytext.png";
+  import fileIcon from "~/assets/file.png";
+  import saveIcon from "~/assets/save.png";
+  import backIcon from "~/assets/back.png";
+</script>
+
 <div class="tools">
   <div class="tool-row">
-    <div class="btn">Download</div>
+    <div class="btn"><img src={loadIcon} alt="Download" title="Download" /></div>
     <div class="full">Assets gathered</div>
   </div>
   <div class="tool-row">
-    <div class="btn">&lt;</div>
+    <div class="btn"><img src={leftIcon} alt="Left" title="Left" /></div>
     <div class="full">The Bulb</div>
-    <div class="btn">&gt;</div>
+    <div class="btn"><img src={rightIcon} alt="Right" title="Right" /></div>
   </div>
   <div class="tool-row">
-    <div class="btn">Search</div>
-    <div class="btn">Add</div>
-    <div class="btn">Add Multiple</div>
-    <div class="btn">Rename</div>
-    <div class="btn">Reorder</div>
-    <div class="btn">Copy Parts</div>
-    <div class="btn">Copy Text</div>
-    <div class="btn">File</div>
+    <div class="btn"><img src={searchIcon} alt="Search" title="Search" /></div>
+    <div class="btn"><img src={addIcon} alt="Add" title="Add" /></div>
+    <div class="btn"><img src={addmultipleIcon} alt="Add Multiple" title="Add Multiple" /></div>
+    <div class="btn"><img src={renameIcon} alt="Rename" title="Rename" /></div>
+    <div class="btn"><img src={reorderIcon} alt="Reorder" title="Reorder" /></div>
+    <div class="btn"><img src={copypartsIcon} alt="Copy Parts" title="Copy Parts" /></div>
+    <div class="btn"><img src={copytextIcon} alt="Copy Text" title="Copy Text" /></div>
+    <div class="btn"><img src={fileIcon} alt="File" title="File" /></div>
   </div>
   <div class="tool-row">
-    <div class="btn">Save</div>
+    <div class="btn"><img src={saveIcon} alt="Save" title="Save" /></div>
     <div class="full">1 unsaved</div>
-    <div class="btn">Back</div>
+    <div class="btn"><img src={backIcon} alt="Back" title="Back" /></div>
   </div>
 </div>
 
@@ -37,6 +53,12 @@
         width: 25px;
         height: 25px;
         overflow: hidden;
+        text-align: text;
+
+        img {
+          width: 100%;
+          height: 100%;
+        }
       }
 
       .full {
